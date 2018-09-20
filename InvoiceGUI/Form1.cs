8ed117fb4cdda8072a6c143a00a938e801087735
SrinivasanMuthukannu <35187294+SrinivasanMuthukannu@users.noreply.github.com>
2018-09-20 09:38:00 +0530

    Added files
    Generate Invi

---
+++ InvoiceGUI/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Invoicer.GUI;
+
+namespace InvoiceGUI
+{
+    public partial class Form1 : Form
+    {
+        DataTable dtexcel = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = string.Empty;
+            string fileExt = string.Empty;
+            OpenFileDialog file = new OpenFileDialog(); //open dialog to choose file  
+            if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK) //if there is a file choosen by the user  
+            {
+                filePath = file.FileName; //get the path of the file  
+                fileExt = Path.GetExtension(filePath); //get the file extension  
+                if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
+                {
+                    try
+                    {
+                        DataTable dtExcel = new DataTable();
+                        dtExcel = ReadExcel(filePath, fileExt); //read excel file  
+                        dataGridView1.Visible = true;
+                        dataGridView1.DataSource = dtExcel;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please choose .xls or .xlsx file only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error); //custom messageBox to show error  
+                }
+            }  
+        }
+
+        public DataTable ReadExcel(string fileName, string fileExt)
+        {
+            string conn = string.Empty;
+            
+            if (fileExt.CompareTo(".xls") == 0)
+                conn = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';"; //for below excel 2007  
+            else
+                conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=NO';"; //for above excel 2007  
+            using (OleDbConnection con = new OleDbConnection(conn))
+            {
+                try
+                {
+                    OleDbDataAdapter oleAdpt = new OleDbDataAdapter("select * from [Sheet1$]", con); //here we read data from sheet1  
+                    oleAdpt.Fill(dtexcel); //fill excel data into dataTable  
+                }
+                catch { }
+            }
+            return dtexcel;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataView dv = new DataView(dtexcel);
+            dv.RowFilter = "F3"+ " LIKE '%" + BillNumber.Text + "%'";
+
+            //BindingSource bs = new BindingSource();
+            //bs.DataSource = dataGridView1.DataSource;
+            //bs.Filter = dataGridView1.Columns[2].HeaderText.ToString() + " LIKE '%" + BillNumber.Text + "%'";
+            dataGridView1.DataSource = dv;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataView dv = new DataView(dtexcel);
+            dv.RowFilter = "F3" + " LIKE '%" + BillNumber.Text + "%'";
+            DataTable dt = new DataTable();
+            dt = dv.ToTable();
+            new Process().Go(dt, BillNumber.Text);
+        }
+
+       
+    }
+}
---
+++ InvoiceGUI/Process.cs
@@ -0,0 +1,82 @@
+﻿using Invoicer.Models;
+using Invoicer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Invoicer.GUI
+{
+    public class Process
+    {
+        public void Go(DataTable dt,string BillNumber)
+        {
+            new InvoicerApi(SizeOption.A4, OrientationOption.Landscape, "RS.", BillNumber)
+                .TextColor("#CC0000")
+                .BackColor("#FFD6CC")
+                .Image(@"..\..\image\sss_logo.png", 125, 27)
+                .Company(Address.Make("FROM", new string[] { "SSS AGENCY", "MANGALAM", "PERAMBALUR", "PH : 99947 05803", "GSTIN : 33CXJPS0061H1ZJ" }, "GSTIN : 33CXJPS0061H1ZJ", ""))
+                .Client(Address.Make("BILLING TO", GetCustomerName(dt)))
+                .Items(GetItemRows(dt))
+                .Totals(GetTotalRow(dt))
+                .Details(new List<DetailRow> {
+                    DetailRow.Make("PAYMENT INFORMATION", "Make all cheques payable to SSS Agency.", "", "If you have any questions concerning this invoice.", "", "Thank you for your business.")
+                })
+                .Footer("SSS Agency")
+                .Save();
+        }
+
+        private List<TotalRow> GetTotalRow(DataTable dt)
+        {
+            List<TotalRow> TotalRowList = new List<TotalRow>();
+            Decimal subTotal = 0;
+            decimal Gst = 0;
+            
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                subTotal += Convert.ToDecimal(dt.Rows[i]["F24"]);
+            }
+            Gst = ((subTotal * 10) / 100);
+            TotalRow totalrow = new TotalRow();
+            totalrow.Name = "Sub Total";            
+            totalrow.Value = subTotal;
+            TotalRowList.Add(totalrow);
+
+            TotalRow totalrow1 = new TotalRow();            
+            totalrow1.Name = "GST 10%";
+            totalrow1.Value = Convert.ToDecimal(Gst);
+            TotalRowList.Add(totalrow1);
+
+            TotalRow totalrow2 = new TotalRow();
+            totalrow2.Name = "Total";
+            totalrow2.Value = subTotal + Convert.ToDecimal(Gst);
+            TotalRowList.Add(totalrow2);
+            return TotalRowList;           
+        }
+
+        private string[] GetCustomerName(DataTable dt)
+        {
+            return new string[] { Convert.ToString(dt.Rows[0]["F6"]), "GSTIN/UIN :" + Convert.ToString(dt.Rows[0]["F7"]) };
+        }
+
+        private List<ItemRow> GetItemRows(DataTable dt)
+        {
+            List<ItemRow> ItemRowList = new List<ItemRow>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                ItemRow itemrow = new ItemRow();
+                itemrow.Name = Convert.ToString(dt.Rows[i]["F26"]);
+                itemrow.Description = string.Empty;//dt.Rows[i]["F26"].ToString();
+                itemrow.Amount = Convert.ToDecimal(dt.Rows[i]["F9"]);
+                itemrow.Price = Convert.ToDecimal(dt.Rows[i]["F9"]);
+                itemrow.VAT = Convert.ToDecimal(dt.Rows[i]["F21"]);
+                itemrow.Total = Convert.ToDecimal(dt.Rows[i]["F24"]);
+                ItemRowList.Add(itemrow);
+            }
+            return ItemRowList;
+        }
+
+    }
+}
